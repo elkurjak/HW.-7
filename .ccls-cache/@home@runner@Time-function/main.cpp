b1d915fc07082c411a3f081e9a8c0a1b70a15370
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-08 03:16:17 +0000

    Added creator comments and got a working function.

--- .ccls-cache/@home@runner@Time-function/main.cpp
+++ .ccls-cache/@home@runner@Time-function/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
+//declare parameter for the time functions
 void time(int seconds, int hours, int minutes, int secs){
- 
- 
-
+  //user input
+ cin>>seconds;
+  //formulas for converting seconds
+  hours= (seconds/3600) % 60;
+  minutes = (seconds /60) % 60;
+  secs = seconds % 60;
 }
 
   
 
 
 int main() {
+  //declare variables again
   int seconds, hours, minutes, secs;
+  //call funtion
   time(seconds, hours, minutes, secs);
 
 }
