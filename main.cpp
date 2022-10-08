3b059183652734c579ddf226114263efb8639608
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-08 03:12:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void time(int seconds, int hours, int minutes, int secs){
+ 
+ 
+
+}
+
+  
+
+
+int main() {
+  int seconds, hours, minutes, secs;
+  time(seconds, hours, minutes, secs);
+
+}
